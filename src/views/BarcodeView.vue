dfc0d6dd26cf848820d0527c69e7fe81c4222782
t_sunwentao <sunwentao@fenrir-tec.com>
2025-01-10 18:04:37 +0800

    data storage

--- src/views/BarcodeView.vue
+++ src/views/BarcodeView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import VBarcode from '../components/VBarcode.vue'
+
+const barcodeValue = sessionStorage.getItem('barcode-value')
 </script>
 
 <template>
-  <h1>Barcode V2</h1>
-  <VBarcode value="This is Barcode View V2" />
+  <h1>Barcode V3</h1>
+  <VBarcode :value="barcodeValue!" />
   <RouterLink to="/" replace class="underline text-blue-300">
     Back Home
   </RouterLink>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
 import VBarcode from '../components/VBarcode.vue'
+
+const router = useRouter()
+localStorage.setItem('barcode-value', 'data from localStorage')
+const barcodeValue = localStorage.getItem('barcode-value')
+
+function toBarcodeView() {
+  sessionStorage.setItem('barcode-value', 'data from sessionStorage')
+  router.push('/barcode')
+}
 </script>
 
 <template>
-  <h1>Home V2</h1>
-  <VBarcode value="This is Home View V2" />
-  <RouterLink to="/barcode" class="underline text-blue-300">
+  <h1>Home V3</h1>
+  <section class="border p-2 rounded w-max my-4">
+    <h2>data from localStorage</h2>
+    <VBarcode :value="barcodeValue!" />
+  </section>
+  <button class="underline text-blue-300" @click="toBarcodeView">
     To Barcode page
-  </RouterLink>
+  </button>
 </template>
